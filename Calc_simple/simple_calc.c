c39cf313b63fcadf642510e3b6d8105fd8ea3dd5
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2020-10-30 12:52:42 +0000

    Simple calc
    Calc sistem simple

---
+++ Calc_simple/simple_calc.c
@@ -0,0 +1,27 @@
+#include <stdio.h. >
+
+
+
+int main(){
+
+ int a, b, c, d;
+ //Imput
+ printf("Digite o primeiro numero:\n");
+ scanf("%d",&a);
+
+ printf("Digite o segundo numero:\n");
+ scanf("%d",&b);
+
+ printf("Digite o terceiro numero:\n");
+ scanf("%d",&c);
+
+ //Clear cmd
+ system ("cls");
+ //Calc operation
+ d = a + b + c;
+ //Output
+ printf("Resultado: %d",d);
+
+ return 0;
+
+}
